109b0e5a0ab5e856505857ebe5a21b847c459bd9
Tawedzegwa Katakwa <tkatakwa@cyestims.com>
2023-06-29 22:41:19 +0200

    add registration token fix

--- NETCoreBot/Program.cs
+++ NETCoreBot/Program.cs
@@ -74,6 +74,10 @@ namespace NETCoreBot
                 Environment.GetEnvironmentVariable("BOT_NICKNAME")
                 ?? Configuration.GetSection("BotNickname").Value;
 
+            var token =
+                Environment.GetEnvironmentVariable("Token") ??
+                Environment.GetEnvironmentVariable("REGISTRATION_TOKEN");
+
             var port = Configuration.GetSection("RunnerPort");
 
             var url = ip + ":" + port.Value + "/runnerhub";
@@ -222,7 +226,7 @@ namespace NETCoreBot
                 return Task.CompletedTask;
             };
 
-            await connection.InvokeAsync("Register", botNickname);
+            await connection.InvokeAsync("Register", token, botNickname);
             while (connection.State == HubConnectionState.Connected)
             {
                 var state = botService.GetBotState();
